d3fa0f593621fd14c472dad57a2d62a0cdc85ce4
Samy Echarif <s.echarif@greenisland.be>
2023-11-30 15:56:02 +0100

    wip: htmx, sse

--- cmds.go
+++ cmds.go
@@ -3,11 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"html/template"
+
+	"time"
+
+	"math/rand"
+	"net/http"
 	"os"
 	"os/exec"
+	"runtime"
 	"sort"
 	"strings"
-	"sync"
+
+	// "sync"
+
+	// "time"
+
+	// "golang.org/x/net/websocket"
 
 	"github.com/charmbracelet/bubbles/list"
 	log "github.com/sirupsen/logrus"
@@ -44,9 +56,9 @@ func Native2(file_path string) string {
 }
 
 func Native3(str string, combo Combo) {
+	time.Sleep(2 * time.Second)
 	log.SetLevel(log.DebugLevel)
-	cmd := exec.Command("/bin/zsh", "-c",
-		str)
+	cmd := exec.Command("/bin/zsh", "-c", str)
 
 	// create a pipe for the output of the script
 	cmdReader, err := cmd.StdoutPipe()
@@ -60,12 +72,14 @@ func Native3(str string, combo Combo) {
 	scanner := bufio.NewScanner(cmdReader)
 	go func() {
 		for scanner.Scan() {
+			chans[combo] <- scanner.Text()
 			if strings.Contains(scanner.Text(), "Error") {
 				log.Errorf("[%-4s|%12s] %s\n", combo.env, combo.tenant, scanner.Text())
 			} else {
 				log.Infof("[%-4s|%12s] %s\n", combo.env, combo.tenant, scanner.Text())
 			}
 		}
+		close(chans[combo])
 	}()
 
 	err = cmd.Start()
@@ -179,6 +193,10 @@ var rootCmd = &cobra.Command{
 			return nil
 		}
 
+		for combo := range m.selected {
+			chans[combo] = make(chan string)
+		}
+
 		list := []list.Item{}
 		for _, v := range config.Scripts {
 			list = append(list, item(v.Name))
@@ -198,6 +216,20 @@ var rootCmd = &cobra.Command{
 			}
 		}
 
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// fmt.Println("request received /")
+			// sls := make([][]string, 0)
+			// for combo := range m.selected {
+			// 	sl := ChanToSlice[string](chans[combo])
+			// 	sls = append(sls, sl)
+			// }
+			// fmt.Println(sls)
+			tmpl := template.Must(template.ParseFiles("index.html"))
+			tmpl.Execute(w, nil)
+		})
+
+		http.HandleFunc("/random", chanHandler)
+
 		if command.Sequentially {
 			fmt.Printf("Running %s sequentially for all selected profiles\n", command.Name)
 			for selected := range m.selected {
@@ -208,19 +240,29 @@ var rootCmd = &cobra.Command{
 			}
 		} else {
 			fmt.Printf("Running %s concurrently for all selected profiles\n", command.Name)
-			var wg = sync.WaitGroup{}
+			// var wg = sync.WaitGroup{}
 
 			for selected := range m.selected {
 				str := generateEnvVariables(selected, m.available[selected], config, command.Command)
 				// fmt.Println(str)
-				wg.Add(1)
+				// wg.Add(1)
 				go func(selected Combo) {
-					defer wg.Done()
+					// defer wg.Done()
+					// time.Sleep(10 * time.Second)
 					Native3(str, selected)
 				}(selected)
 			}
-			wg.Wait()
+			// for m := range ch {
+			// 	// We got a message. Do the usual SSE stuff.
+			// 	fmt.Println("msg received in chan", m)
+			// }
+			// wg.Wait()
+			// fmt.Println(ch)
 		}
+		fmt.Println("listen")
+		log.Fatal(http.ListenAndServe(":8000", nil))
+
+		open("localhost:8000")
 
 		return nil
 	},
@@ -244,4 +286,91 @@ func init() {
 	)
 	// fmt.Println("help")
 	rootCmd.AddCommand(helpCmd)
+}
+
+var chans = make(map[Combo](chan string))
+
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
+// type Server struct {
+//     conns map[*websocket.Conn]bool
+// }
+
+// func NewServer() *Server {
+//     return &Server{
+//         conns: make(map[*websocket.Conn]bool),
+//     }
+// }
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+
+	// send a random number every 2 seconds
+	for {
+		rand.Seed(time.Now().UnixNano())
+		fmt.Fprintf(w, "data: %d \n\n", rand.Intn(100))
+		w.(http.Flusher).Flush()
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func chanHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+
+	// ch := make(chan string)
+
+	for _, ch := range chans {
+		fmt.Println("here1")
+		go func(ch chan string) {
+
+			fmt.Println("here")
+			for {
+				select {
+				case <-r.Context().Done():
+					// User closed the connection. We are out of here.
+					return
+				case m, err := <-ch:
+					// We got a message. Do the usual SSE stuff.
+					if err {
+						fmt.Println(err)
+						return
+					}
+
+					fmt.Println("here>", m)
+					fmt.Fprintf(w, "data: %s \n\n", m)
+					w.(http.Flusher).Flush()
+				}
+			}
+		}(ch)
+
+	}
+}
+
+func ChanToSlice[T any](ch chan T) []T {
+	ts := make([]T, 0)
+	for t := range ch {
+		ts = append(ts, t)
+	}
+	return ts
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/charmbracelet/bubbletea v0.24.2
 	github.com/charmbracelet/lipgloss v0.9.1
 	github.com/mattn/go-runewidth v0.0.15
+	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.17.0
 )
@@ -32,7 +33,6 @@ require (
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.10.0 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
@@ -43,7 +43,7 @@ require (
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
